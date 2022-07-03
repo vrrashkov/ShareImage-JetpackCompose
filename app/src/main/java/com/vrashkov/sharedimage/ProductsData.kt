acf5e1e7e37b2d7e979d0821a9c124cce11f4bb9
Vladislav Rashkov <mrcrozer@gmail.com>
2022-07-03 16:57:55 +0300

    init

---
+++ app/src/main/java/com/vrashkov/sharedimage/ProductsData.kt
@@ -0,0 +1,10 @@
+package com.vrashkov.sharedimage
+
+data class ProductsData(
+    val productList: List<Int> = listOf(
+        R.drawable.bags_2, R.drawable.bags_3,
+        R.drawable.shoe_1, R.drawable.shoe_2, R.drawable.shoe_3, R.drawable.shoe_4,
+        R.drawable.bags_2, R.drawable.bags_3,
+        R.drawable.shoe_1, R.drawable.shoe_2, R.drawable.shoe_3, R.drawable.shoe_4
+    )
+)
